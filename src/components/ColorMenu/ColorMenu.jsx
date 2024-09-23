54f0e105d3fdad24455ce584840ef3150b57fae8
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-09-23 18:29:57 -0500

    Add menu-button for testing

--- src/components/ColorMenu/ColorMenu.jsx
+++ src/components/ColorMenu/ColorMenu.jsx
@@ -1,1 +1,14 @@
 import "./ColorMenu.css";
+import random from "./random.svg";
+
+export default function ColorMenu() {
+	return (
+		<div className="color-menu">
+			<div className="random">
+				<button className="random-button">
+					<img src={random} alt="Random Button" className="random-image" />
+				</button>
+			</div>
+		</div>
+	);
+}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
+import ColorMenu from "../ColorMenu/ColorMenu";
 import "./Header.css";
 
 export default function Header() {
 	return (
 		<>
 			<div>hellop htere</div>
+
+			<ColorMenu />
 		</>
 	);
 }
