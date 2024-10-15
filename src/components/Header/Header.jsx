f577c3ba6cfa438f86bac4ab8a4a65871e41881d
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-10-14 19:39:33 -0500

    Add header, subheader, add Poppins and Roboto fonts

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,13 +6,13 @@ export default function Header({ colors }) {
 	return (
 		<>
 			<Navigation colors={colors} />
-
 			<div className="header" style={{ backgroundColor: colors.background }}>
 				<h1 className="header__title" style={{ color: colors.primary }}>
-					Navigate Your{" "}
-					<span style={{ color: colors.secondary }}>Perfect Website</span> Color
-					Scheme
+					Design with <span style={{ color: colors.secondary }}>Harmony</span>{" "}
 				</h1>
+				<p className="header__sub-text" style={{ color: colors.secondary }}>
+					Create balanced color schemes for a seamless user experience.
+				</p>
 			</div>
 		</>
 	);
